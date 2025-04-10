94cb949c6f80bb330c152df71aa611af576e8680
qazxsw1240 <qazxsw010528@gmail.com>
2025-04-10 22:35:26 +0900

    initialize project && add Chapter1 project

---
+++ Chapter1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareDesign
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

6795169a6d02aa184e5b1a769f19223330c4399a
qazxsw1240 <qazxsw010528@gmail.com>
2025-04-25 18:45:22 +0900

    Add Restaurant project

---
+++ Restaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

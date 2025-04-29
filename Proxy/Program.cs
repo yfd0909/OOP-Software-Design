44bed2374dda1400239fd3a2e8e3aac9c99ee14b
qazxsw1240 <qazxsw010528@gmail.com>
2025-04-30 02:08:59 +0900

    Add Program class in Proxy Project

---
+++ Proxy/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Proxy
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

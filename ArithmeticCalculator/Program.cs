9a4db9911079381272fa75aa0b8fb760a9c9179d
qazxsw1240 <qazxsw010528@gmail.com>
2025-05-13 11:31:47 +0900

    Update Program.cs in ArithmeticCalculator project

--- ArithmeticCalculator/Program.cs
+++ ArithmeticCalculator/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.IO;
 
 namespace ArithmeticCalculator
 {
     public static class Program
     {
+        private static readonly string[] TestCases = [
+            "10 + 3 * 5 / (16 - 4)",
+            "(17 + 10) * 3 / 9",
+            "-2 + 5",
+            "3 + (-1)"
+        ];
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            foreach (string testCase in TestCases)
+            {
+                TokenGenerator tokenGenerator = new(testCase);
+                SyntaxTreeBuilder builder = new(tokenGenerator.Generate());
+                SyntaxNode topNode = builder.Build();
+                Console.WriteLine($"Evaluate \"{testCase}\": {topNode.Evaluate()}");
+            }
         }
     }
 }
